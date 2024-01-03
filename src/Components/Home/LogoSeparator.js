664f03dbd02d9824d7c7cc14ccf841274d747e88
FUZON12658 <fuzon12658@gmail.com>
2024-01-04 00:46:18 +0545

    minor changes implemented

--- src/Components/Home/LogoSeparator.js
+++ src/Components/Home/LogoSeparator.js
@@ -1,5 +1,5 @@
+import { Link } from "react-router-dom"
 import heroImage from "../../Images/resortLogo.png"
-
 
 export default () => {
   return (
@@ -25,11 +25,13 @@ export default () => {
                   ipsa enim quibusdam illo laboriosam omnis. Labore itaque illum
                   distinctio eum neque!
                 </p>
+                <Link to="tel:9805843762">
                 <button type="button"
                   class="rounded-full border-2 border-neutral-50 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-100 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200"
                   data-te-ripple-init data-te-ripple-color="light">
-                  Sign up now
+                  Call Now
                 </button>
+                </Link>
               </div>
             </div>
           </div>
